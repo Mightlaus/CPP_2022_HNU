9399681e36a2fdaa629f1b0f61066b2c87614121
Ding Ruogu <78856759+Mightlaus@users.noreply.github.com>
2022-10-08 20:02:48 +0800

    大数乘法全面完成

--- main.cpp
+++ main.cpp
@@ -2,40 +2,30 @@
 #include <string>
 #include <vector>
 
-# define MAXSIZE 20
+#define MAXSIZE 200
 
 using namespace std;
 
 // erase head zero of a string
-string erase_zero(const vector<int>& numVec){
+string erase_zero(const vector<int> &numVec) {
     string result;
 
     bool beginAdd = false;
-    for(int i=numVec.size()-1; i>=0; --i){
+    for (int i = numVec.size() - 1; i >= 0; --i) {
         int num = numVec[i];
-        if(num){
+        if (num) {
             beginAdd = true;
         }
 
-        if(beginAdd){
-            result += char ('0' + num) ;
+        if (beginAdd) {
+            result += char('0' + num);
         }
     }
-
     return result;
 }
 
-// returns the carry result of adding two numbers
-int carry_out(int num1, int num2, int carryIn = 0) {
-    if (num1 + num2 + carryIn > 9) {
-        return (num1 + num2 + carryIn) / 10;
-    } else {
-        return 0;
-    }
-}
-
 // return the point of rearranged(reversed) int vector<int>
-vector<int> divide_nums(const string &strNums, int vSize = MAXSIZE) {
+vector<int> str2vector(const string &strNums, int vSize = 2 * MAXSIZE) {
     vector<int> rearranged(vSize, 0);
 
 
@@ -48,36 +38,76 @@ vector<int> divide_nums(const string &strNums, int vSize = MAXSIZE) {
 }
 
 // add two huge number
-string hugeAdd(const string& hugeNum1, const string& hugeNum2, int carryIn=0){
+string huge_add(const string &hugeNum1, const string &hugeNum2, int carryIn = 0) {
 
     int maxSize = max(hugeNum1.size(), hugeNum2.size());
-    vector<int> addResult(maxSize+1, 0);
+    vector<int> addResult(maxSize + 1, 0);
 
-    auto add1 = divide_nums(hugeNum1);
-    auto add2 = divide_nums(hugeNum2);
+    auto add1 = str2vector(hugeNum1, maxSize+1);
+    auto add2 = str2vector(hugeNum2, maxSize+1);
 
-    for (int i = 0, curCarry = carryIn; i < maxSize; ++i) {
+    int curCarry = carryIn;
+    for (int i = 0; i < maxSize; ++i) {
         addResult[i] = (curCarry + add1[i] + add2[i]) % 10;
-        curCarry = carry_out(add1[i], add2[i], curCarry);
+        curCarry = (curCarry + add1[i] + add2[i]) / 10;
     }
 
-    string result = erase_zero(addResult);
+    addResult[maxSize] = curCarry;
+
+    return erase_zero(addResult);
+}
+
+// multiply a huge number with a single number;
+string one_huge_multiply(const string &hugeNum, int sglNum, int carryIn = 0) {
+    auto vHuge = str2vector(hugeNum, hugeNum.size()+1);
+
+    int size = hugeNum.size();
+    vector<int> result(size + 1, 0);
+    int curCarry = carryIn;
+    for (int i = 0; i < hugeNum.size(); ++i) {
+        result[i] = (vHuge[i] * sglNum + curCarry) % 10;
+        curCarry = (vHuge[i] * sglNum + curCarry) / 10;
+    }
+
+    result[size] = curCarry;
+
+    return erase_zero(result);
+}
+
+// huge multiply for two huge numbers
+string huge_multiply(string num1, string num2) {
+    auto vNum1 = str2vector(num1, num1.size());
+    auto vNum2 = str2vector(num2, num2.size());
+
+    vector<string> multiContiner;
+    for (int i = 0; i < vNum2.size(); ++i) {
+        string curResult;
+        curResult = one_huge_multiply(num1, vNum2[i]);
+        for (int j = 0; j < i; ++j) {
+            curResult += '0';
+        }
+        multiContiner.push_back(curResult);
+    }
+
+    string result = "0";
+    for (auto &i: multiContiner) {
+        result = huge_add(result, i);
+    }
+
     return result;
 }
 
-
 int main() {
     // generate two vectors to store two huge number
-//    string str;
-//    cin >> str;
-//    auto hugeNum1= divide_nums(str);
-//    cin >> str;
-//    auto hugeNum2 = divide_nums((str));
+    string str;
+    cin >> str;
+    string hugeNum1 = str;
+    cin >> str;
+    string hugeNum2 = str;
 
-    string a = "123456789098765432";
-    string b = "234123432556789";
+    string r = huge_multiply(hugeNum1, hugeNum2);
 
-    auto r = hugeAdd(a, b);
+    cout << r << endl;
 
     return 0;
 }
--- 小班任务/long_multiply/longMultiply.cpp
+++ 小班任务/long_multiply/longMultiply.cpp
@@ -1,3 +1,113 @@
-//
-// Created by Ding Ruogu on 2022/10/6.
-//
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define MAXSIZE 2000000000000
+
+using namespace std;
+
+// erase head zero of a string
+string erase_zero(const vector<int> &numVec) {
+    string result;
+
+    bool beginAdd = false;
+    for (int i = numVec.size() - 1; i >= 0; --i) {
+        int num = numVec[i];
+        if (num) {
+            beginAdd = true;
+        }
+
+        if (beginAdd) {
+            result += char('0' + num);
+        }
+    }
+    return result;
+}
+
+// return the point of rearranged(reversed) int vector<int>
+vector<int> str2vector(const string &strNums, int vSize = 2 * MAXSIZE) {
+    vector<int> rearranged(vSize, 0);
+
+
+    for (int strPlc = strNums.size() - 1, arrayPlc = 0; strPlc >= 0; strPlc--) {
+        rearranged[arrayPlc] = strNums[strPlc] - '0';
+        arrayPlc++;
+    }
+
+    return rearranged;
+}
+
+// add two huge number
+string huge_add(const string &hugeNum1, const string &hugeNum2, int carryIn = 0) {
+
+    int maxSize = max(hugeNum1.size(), hugeNum2.size());
+    vector<int> addResult(maxSize + 1, 0);
+
+    auto add1 = str2vector(hugeNum1);
+    auto add2 = str2vector(hugeNum2);
+
+    int curCarry = carryIn;
+    for (int i = 0; i < maxSize; ++i) {
+        addResult[i] = (curCarry + add1[i] + add2[i]) % 10;
+        curCarry = (curCarry + add1[i] + add2[i]) / 10;
+    }
+
+    addResult[maxSize] = curCarry;
+
+    return erase_zero(addResult);
+}
+
+// multiply a huge number with a single number;
+string one_huge_multiply(const string &hugeNum, int sglNum, int carryIn = 0) {
+    auto vHuge = str2vector(hugeNum);
+
+    int size = hugeNum.size();
+    vector<int> result(size + 1, 0);
+    int curCarry = carryIn;
+    for (int i = 0; i < hugeNum.size(); ++i) {
+        result[i] = (vHuge[i] * sglNum + curCarry) % 10;
+        curCarry = (vHuge[i] * sglNum + curCarry) / 10;
+    }
+
+    result[size] = curCarry;
+
+    return erase_zero(result);
+}
+
+// huge multiply for two huge numbers
+string huge_multiply(string num1, string num2) {
+    auto vNum1 = str2vector(num1, num1.size());
+    auto vNum2 = str2vector(num2, num2.size());
+
+    vector<string> multiContiner;
+    for (int i = 0; i < vNum2.size(); ++i) {
+        string curResult;
+        curResult = one_huge_multiply(num1, vNum2[i]);
+        for (int j = 0; j < i; ++j) {
+            curResult += '0';
+        }
+        multiContiner.push_back(curResult);
+    }
+
+    string result = "0";
+    for (auto &i: multiContiner) {
+        result = huge_add(result, i);
+    }
+
+    return result;
+}
+
+int main() {
+    // generate two vectors to store two huge number
+    string str;
+    cin >> str;
+    string hugeNum1 = str;
+    cin >> str;
+    string hugeNum2 = str;
+
+    string r = huge_multiply(hugeNum1, hugeNum2);
+
+    cout << r << endl;
+
+    return 0;
+}
