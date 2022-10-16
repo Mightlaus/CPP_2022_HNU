7360562c77c49f1cfa911d798e762790e31d69ad
Ding Ruogu <78856759+Mightlaus@users.noreply.github.com>
2022-10-16 12:05:28 +0800

    完善飞机起飞时间安排

--- CG/练习四_数组_强化/06_飞机起飞时间安排.cpp
+++ CG/练习四_数组_强化/06_飞机起飞时间安排.cpp
@@ -10,27 +10,39 @@ using namespace std;
 int main() {
     int n, s;
     cin >> n >> s;  // n:plane nums, s:minimum permitted time
+//    cout << n << " " << s << endl;
     vector<int> landTime;
 
     for (int i = 0; i < n; ++i) {
         int hour, min;
         cin >> hour >> min;
+//        cout << hour << " " << min << endl;
         landTime.push_back(total_min(hour, min));
     }
 
-    int former = -1;
-    for (int i = 0; i < n; ++i) {
+    // 第一班之前
+    if (landTime[n - 1] + 1 + 1 * s > 24 * 60 && (landTime[n - 1] + 2 + 2 * s - 24 * 60 < landTime[0])) {
+        int hour = (1 + landTime[n - 1] + s - 24 * 60) / 60;
+        int min = (1 + landTime[n - 1] + s - 24 * 60) % 60;
+        cout << hour << " " << min;
+        return 0;
+    }
+
+    // 第一班与最后一班之间
+    int former = landTime[0];
+    for (int i = 1; i < n; ++i) {
         if (landTime[i] - former >= 2 + 2 * s) {
             int hour = (1 + former + s) / 60;
             int min = (1 + former + s) % 60;
             cout << hour << " " << min;
             return 0;
         }
-
         former = landTime[i];
     }
 
-    int hour = (1 + landTime[n - 1]) / 60;
-    int min = (1 + landTime[n - 1]) % 60;
+    //最后一班之后
+    int hour = (1 + former + s) / 60;
+    int min = (1 + former + s) % 60;
+    hour = hour == 24 ? 0 : hour;
     cout << hour << " " << min;
 }
--- main.cpp
+++ main.cpp
@@ -1,59 +1,48 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
- 
+int total_min(int hour, int min) {
+    return hour * 60 + min;
+}
+
 using namespace std;
 
-int *getP(void);
+int main() {
+    int n, s;
+    cin >> n >> s;  // n:plane nums, s:minimum permitted time
+//    cout << n << " " << s << endl;
+    vector<int> landTime;
 
-int *getQ(void);
+    for (int i = 0; i < n; ++i) {
+        int hour, min;
+        cin >> hour >> min;
+//        cout << hour << " " << min << endl;
+        landTime.push_back(total_min(hour, min));
+    }
 
-int main()
+    // 第一班之前
+    if (landTime[n - 1] + 1 + 1 * s > 24 * 60 && (landTime[n - 1] + 2 + 2 * s - 24 * 60 < landTime[0])) {
+        int hour = (1 + landTime[n - 1] + s - 24 * 60) / 60;
+        int min = (1 + landTime[n - 1] + s - 24 * 60) % 60;
+        cout << hour << " " << min;
+        return 0;
+    }
 
-{
+    // 第一班与最后一班之间
+    int former = landTime[0];
+    for (int i = 1; i < n; ++i) {
+        if (landTime[i] - former >= 2 + 2 * s) {
+            int hour = (1 + former + s) / 60;
+            int min = (1 + former + s) % 60;
+            cout << hour << " " << min;
+            return 0;
+        }
+        former = landTime[i];
+    }
 
-    int *p, *q;
-
-    p=getP();
-
-    cout<<p[0]<<" "<<p[1]<<endl;
-
-    q=getQ();
-
-    cout<<q[0]<<" "<<q[1]<<endl;
-	string str("123456sdf");
-	
-	
-    return 0;
-    
-   
-
-}
-
-
-
-int *getP(void)
-
-{
-
-    int *s=new int[2];
-
-    s[0]=1, s[1]=2;
-
-    return s;
-
-}
-
-
-
-int *getQ(void)
-
-{
-
-    int s[2];
-
-    s[0]=1, s[1]=2;
-
-    return s;
-
+    //最后一班之后
+    int hour = (1 + former + s) / 60;
+    int min = (1 + former + s) % 60;
+    hour = hour == 24 ? 0 : hour;
+    cout << hour << " " << min;
 }
