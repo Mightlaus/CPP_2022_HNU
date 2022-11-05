d242ae1d9f670920df628b159434dfbdf44a9ab3
Ding Ruogu <78856759+Mightlaus@users.noreply.github.com>
2022-11-05 11:22:31 +0800

    顺利运行，还缺少一个中止条件

--- CG/练习七_字符串/13_激光镜像.cpp
+++ CG/练习七_字符串/13_激光镜像.cpp
@@ -5,42 +5,74 @@
 
 using namespace std;
 
-// ------>y(m)
-// |
-// |
-// |
-// x(n)
+/*
+         ------>x(n)
+         |
+         |
+         |
+         y(m)
+ */
 
-// 光线怎么走：分情况讨论
-// 怎么判断进入死循环？（次数够就行）
-
-struct Square{
-    int x;
-    int y;
-    int dirX;
-    int dirY;
-    bool solid;
-    int times;
+struct Square {
+    int x; // 位置x
+    int y; // 位置y
+    int dirX; // 入射方向x，1/-1
+    int dirY; // 入射方向y，1/-1
+    bool solid; // 实心1
+    int times; // 被入射次数
 };
 
-void shoot(Square &square){
+void shoot(Square **matrix, int x, int y) {
+    /*
+                -   -   -   -   -   -   > x
+             | （x)(y)      (x+dirX)(y)
+             | (x)(y+dirY)（x+dirX)(y+dirY)
+             |
+             y
+     */
+
     // todo 加一个边界判断
-    square.times += 1;
+    matrix[x][y].times += 1;
+
+    int dirX = matrix[x][y].dirX;
+    int dirY = matrix[x][y].dirY;
+    int newDirX, newDirY;
+    if (!matrix[x + dirX][y + dirY].solid) { // （x+dirX)(y+dirY)  空：原路返回
+        newDirX = dirX;
+        newDirY = dirY;
+    } else { // （x+dirX)(y+dirY) 非空
+        if (!(matrix[x + dirX][y].solid xor matrix[x][y + dirY].solid)){ // (x+dirX)(y) 同或 (x)(y+dirY) ：原路返回
+            newDirX = -dirX;
+            newDirY = -dirY;
+        } else {
+            if(matrix[x + dirX][y].solid){ // (x+dirX)(y) 非空： x方向反，y方向不变
+                newDirX = -dirX;
+                newDirY = dirY;
+            } else {
+                newDirX = dirX;
+                newDirY = dirY;
+            }
+        }
+    }
+
+    // 改变下一个square属性
+    matrix[x+newDirX][y+newDirY].dirX = newDirX;
+    matrix[x+newDirX][y+newDirY].dirY = newDirY;
 
 }
 
-void show_matrix(Square **i_matrix, int xLen, int yLen){
+void show_matrix(Square **i_matrix, int xLen, int yLen) {
     for (int x = 0; x < xLen; ++x) {
         printf("\tx=%d", x);
     }
-    cout<<endl;
+    cout << endl;
 
     for (int y = 0; y < yLen; ++y) {
         printf("y=%d", y);
         for (int x = 0; x < xLen; ++x) {
             printf("\t %c", i_matrix[x][y].solid ? '*' : '0');
         }
-        cout<<endl;
+        cout << endl;
     }
 
 }
@@ -52,16 +84,16 @@ int main() {
 
 //    Square matrix[m+2][n+2];
 
-    auto **matrix = new Square *[m+2];
-    for (int i = 0; i < m+2; i++) {
-        matrix[i] = new Square[n+2];
+    auto **matrix = new Square *[m + 2];
+    for (int i = 0; i < n + 2; i++) {
+        matrix[i] = new Square[n + 2];
     }
 
     // 初始化matrix
-    for (int x = 0; x < n+2; ++x) {
-        for (int y = 0; y < m+2; ++y) {
+    for (int x = 0; x < n + 2; ++x) {
+        for (int y = 0; y < m + 2; ++y) {
             bool solid = 0;
-            if(x==0 or y==0 or x==n+1 or y==m+1){ // 最外围全部实心
+            if (x == 0 or y == 0 or x == n + 1 or y == m + 1) { // 最外围全部实心
                 solid = 1;
             }
 
@@ -69,15 +101,15 @@ int main() {
         }
     }
 
-//    // 设置实心块
-//    for (int i = 0; i < k; ++i) {
-//        int x,y;
-//        cin>>x>>y;
-//        matrix[x+1][y+1] = Square{x+1, y+1, 0, 0, 1, 0};
-//    }
+    // 设置实心块
+    for (int i = 0; i < k; ++i) {
+        int x, y;
+        cin >> x >> y;
+        matrix[x + 1][y + 1] = Square{x + 1, y + 1, 0, 0, 1, 0};
+    }
 
-    show_matrix(matrix, n+2, m+2);
+    show_matrix(matrix, n + 2, m + 2);
 
-    cout<<0;
+    cout << 0;
 
 }
--- 小班任务/sort/结构体条件排序.cpp
+++ 小班任务/sort/结构体条件排序.cpp
@@ -1,47 +1,156 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-struct WUJIANG{
-    string name;
-    int age;
-    int tili;
-    int wuli;
-    int zhili;
+/*
+         ------>x(n) E
+         |
+         |
+         |
+         y(m) N
+ */
+
+struct Square {
+    int x; // 位置x
+    int y; // 位置y
+    int dirX; // 入射方向x，1/-1
+    int dirY; // 入射方向y，1/-1
+    bool solid; // 实心1
+    int times; // 被入射次数
 };
 
-bool cmp(WUJIANG p1, WUJIANG p2){ // 返回false交换
-    if(p2.wuli!=p1.wuli) return p2.wuli<p1.wuli;
-    if(p2.zhili!=p1.zhili) return p2.zhili < p1.zhili;
-    if(p2.tili!=p1.tili) return p2.tili<p1.tili;
-    if(p2.age!=p1.age) return p2.age > p1.age;
+void shoot(Square **i_matrix, int &x, int &y) {
+    /*
+                -   -   -   -   -   -   > x
+             | （x)(y)      (x+dirX)(y)
+             | (x)(y+dirY)（x+dirX)(y+dirY)
+             |
+             y
+     */
+
+    // todo 加一个边界判断
+    i_matrix[x][y].times += 1;
+
+    int dirX = i_matrix[x][y].dirX;
+    int dirY = i_matrix[x][y].dirY;
+    int newDirX, newDirY;
+    if (!i_matrix[x + dirX][y + dirY].solid) { // （x+dirX)(y+dirY)  空：继续前进
+        newDirX = dirX;
+        newDirY = dirY;
+        x = x + newDirX;
+        y = y + newDirY;
+    } else { // （x+dirX)(y+dirY) 非空
+        if (!(i_matrix[x + dirX][y].solid xor i_matrix[x][y + dirY].solid)) { // (x+dirX)(y) 同或 (x)(y+dirY) ：原路返回
+            newDirX = -dirX;
+            newDirY = -dirY;
+            x = x;
+            y = y;
+        } else {
+            if (i_matrix[x + dirX][y].solid) { // (x+dirX)(y) 非空： x方向反，y方向不变
+                newDirX = -dirX;
+                newDirY = dirY;
+                x = x;
+                y = y+newDirY;
+            } else {
+                newDirX = dirX;
+                newDirY = -dirY;
+                x = x + newDirX;
+                y = y;
+            }
+        }
+    }
+
+
+    // 改变下一个square属性
+    i_matrix[x][y].dirX = newDirX;
+    i_matrix[x][y].dirY = newDirY;
+
+
+}
+
+void show_matrix(Square **i_matrix, int xLen, int yLen) {
+    for (int x = 0; x < xLen; ++x) {
+        printf("\tx=%d", x);
+    }
+    cout << endl;
+
+    for (int y = 0; y < yLen; ++y) {
+        printf("y=%d", y);
+        for (int x = 0; x < xLen; ++x) {
+            printf("\t %c", i_matrix[x][y].solid ? '*' : i_matrix[x][y].times+'0');
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void initialize(Square **i_matrix, int x, int y, string dir) {
+    int dirX, dirY;
+    if (dir[0] == 'N') {
+        dirX = 1;
+    } else {
+        dirX = -1;
+    }
+
+    if (dir[1] == 'E') {
+        dirY = 1;
+    } else {
+        dirY = -1;
+    }
+
+    i_matrix[x][y].dirX = dirX;
+    i_matrix[x][y].dirY = dirY;
+
 }
 
 int main() {
+    int n, m, k;
+    cin >> n >> m >> k; // n rows(x), m columns(y), k black squares
 
-    vector<struct WUJIANG> vec;
 
-    while(1){
-        string name;
-        int age, tili, zhili, wuli;
-        cin >> name;
-        if(name=="#"){
-            break;
+    auto **matrix = new Square *[m + 2];
+    for (int i = 0; i < n + 2; i++) {
+        matrix[i] = new Square[n + 2];
+    }
+
+    // 初始化matrix
+    for (int x = 0; x < n + 2; ++x) {
+        for (int y = 0; y < m + 2; ++y) {
+            bool solid = 0;
+            if (x == 0 or y == 0 or x == n + 1 or y == m + 1) { // 最外围全部实心
+                solid = 1;
+            }
+
+            matrix[x][y] = Square{x, y, 0, 0, solid, 0};
         }
-        cin >> age >> tili >> zhili >> wuli;
-
-        WUJIANG wujiang={name, age, tili, wuli, zhili};
-
-        vec.push_back(wujiang);
     }
 
-    sort(vec.begin(), vec.end(), cmp);
-
-    for(auto x:vec){
-        printf("%s   %d   %d   %d   %d\n",x.name.c_str(),x.age,x.tili,x.zhili,x.wuli);
+    // 设置实心块
+    for (int i = 0; i < k; ++i) {
+        int x, y;
+        cin >> x >> y;
+        matrix[x][y] = Square{x + 1, y + 1, 0, 0, 1, 0};
     }
+
+    show_matrix(matrix, n + 2, m + 2);
+
+    // 准备入射
+    int x, y;
+    string dir;
+    cin >> x >> y >> dir;
+    initialize(matrix, x, y, dir);
+    for (int i = 0; i < 20; ++i) {
+        shoot(matrix, x, y);
+        cout<<"#"<<i;
+        show_matrix(matrix, n+2, m+2);
+    }
+
+
+
+    cout << 0;
 
 }
